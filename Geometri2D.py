d88cba8ce7cdad2d24898895daf64f3a9df7f915
SAHRAENI MAULIDA <49121673+SAHRAENIMAULIDA@users.noreply.github.com>
2019-10-15 21:50:35 +0700

    Geometri2D.py

---
+++ Geometri2D.py
@@ -0,0 +1,23 @@
+import math 
+
+def luasPersegiPanjang (p, l):
+	return p * l
+
+def kelilingPersegiPanjang(p, l):
+	return p * l
+
+def luasBujurSangkar(s):
+	return s * s
+
+def kelilingBujurSangkar(s):
+	return 4 * s
+
+def luasLingkaran(r):
+	return 2 * math.pi * r
+
+def luasSegitiga ( a, t):
+	return ( a * t )/ 2
+
+def kelilingSSegitiga ( a, b, c):
+	return a + b + c
+
