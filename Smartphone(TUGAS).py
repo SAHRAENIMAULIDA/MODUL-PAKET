0c29c132a1a4bc86d1fa9d1732e1e4182f73488f
SAHRAENI MAULIDA <49121673+SAHRAENIMAULIDA@users.noreply.github.com>
2019-10-15 22:23:19 +0700

    Create Smartphone(TUGAS).py

---
+++ Smartphone(TUGAS).py
@@ -0,0 +1,15 @@
+import Smartphone 
+def apple_sp():
+   	return "Ini adalah smartphone apple"
+
+def samsung_sp():
+    return "Ini adalah smartphone samsung"
+
+def vivo_sp():
+    return "Ini adalah smartphone vivo"
+
+def xiomi_sp():
+	return "ini adalah smartphone xiomi"
+
+def oppo_sp():
+	return "ini adalah smartphone oppo"
