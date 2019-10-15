f1af3bbe9bae0c2de33199dfd74c558ac6ec1a3a
SAHRAENI MAULIDA <49121673+SAHRAENIMAULIDA@users.noreply.github.com>
2019-10-15 21:54:27 +0700

    import.py

---
+++ import.py
@@ -0,0 +1,26 @@
+import geometri2D
+def main():
+	p = 10
+	l = 8
+
+	luas = geometri2D.luasPersegiPanjang(p,l)
+	kel = geometri2D.kelilingPersegiPanjang(p,l)
+
+	print ("PERSEGI PANJANG")
+	print("panjang\t :", p)
+	print("Lebar\t:", l)
+	print("Luas\t :", luas)
+	print("Keliling\t = ", kel )
+
+	jarijari = 3
+
+	luas = geometri2D.luasLingkaran(jarijari)
+	kel = geometri2D.kelilingLingkaran(jarijari) 
+
+	print("\nLINGKARAN")
+	print("Jari-jari\t: ", jarijari)
+	print("Luas\t =", luas)
+	print("Keliling\t =", kel)
+
+if __name__ == "__main__":
+	main()
