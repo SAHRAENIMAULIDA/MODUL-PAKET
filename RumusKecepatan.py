fc9cd5c97a507677bbfe0ba1448961b02fcd1cf7
SAHRAENI MAULIDA <49121673+SAHRAENIMAULIDA@users.noreply.github.com>
2019-10-16 18:38:39 +0800

    Create RumusKecepatan.py

---
+++ RumusKecepatan.py
@@ -0,0 +1,15 @@
+import fisika 
+
+def main():
+	s=50
+	t=25
+
+	cepat = fisika.kecepatan(s,t)
+
+	print("Mencari Kecepatan")
+	print("Jarak\t: ", s)
+	print("Waktu\t: ", t)
+	print("Kecepatan\t= ", cepat)
+
+if __name__ == "__main__":
+    main()
